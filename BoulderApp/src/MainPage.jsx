784ed1872d48e64e0a4b95bb83cccfa87da40798
siemh <siemhorst0@gmail.com>
2026-02-16 19:10:41 +0100

    made connection with the database card are standing in a weird position tho

--- BoulderApp/src/MainPage.jsx
+++ BoulderApp/src/MainPage.jsx
@@ -1,13 +1,34 @@
 import RouteCard from "./RouteCard.jsx"
+import {createClient} from "@supabase/supabase-js"
+import {useEffect, useState } from "react"
+
+const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+);
 
 function MainPage(){
+    const [climbingRoutes, setClimbingRoutes] = useState([]);
+
+    useEffect(()=>{
+        getClimbingRoutes();
+    }, []);
+
+    async function getClimbingRoutes() {
+    const { data } = await supabase.from("climbingRoutes").select();
+    setClimbingRoutes(data);
+  }
+
     return(
     <>
         {/*Filled with test data for now this is gonna change to an API from a database*/}
-        <RouteCard id="1" name="Route 1" grade="v2"></RouteCard>
-        <RouteCard id="2" name="Route 2" grade="v7"></RouteCard>
-        <RouteCard id="3" name="Route 3" grade="v1"></RouteCard>
-        <RouteCard id="4" name="Route 4" grade="v4"></RouteCard>
+
+        <ul>
+            {climbingRoutes.map((climbingRoute) => (
+                <RouteCard id={climbingRoute.id} name={climbingRoute.routeName} grade={climbingRoute.grade}></RouteCard>
+            ))}
+        </ul>
+        
+        
     </>
     );
 }
